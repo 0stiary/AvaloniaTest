d82fcefdebcbe548fa88ade241ccf6b9dc3e2cef
Ostiary <ec1o.force@gmail.com>
2022-04-11 12:24:00 +0300

    Refactoring

--- AvaloniaTest/MainWindow.axaml.cs
+++ AvaloniaTest/MainWindow.axaml.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Mime;
 using System.Threading;
 using System.Threading.Tasks;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
+using Avalonia.Media;
 using Avalonia.Media.Imaging;
 using Avalonia.Threading;
 using HtmlAgilityPack;
@@ -19,64 +22,74 @@ namespace AvaloniaTest
 		public MainWindow()
 		{
 			InitializeComponent();
+			PlayListBioContainer.IsVisible = false;
 		}
 
-		private async void ParseButton_OnClick(object? sender, RoutedEventArgs e)
+		private void ParseButton_OnClick(object? sender, RoutedEventArgs e)
 		{
-			//parse url?
+			this.Cursor = new Cursor(StandardCursorType.Wait);
+			var isError = false;
 
-			var html = PlaylistUrl.Text;
-
-			HtmlWeb web = new ();
-
-			var htmlDoc = web.Load(html);
-			
-			// img from page
-			var imgSrc = htmlDoc.DocumentNode.SelectSingleNode("//div[@class='h-100 image']/img").Attributes["src"].Value;
-			PlaylistImage.Source = await BitmapImageFromUrl(imgSrc);
-
-			var productInfoNode = htmlDoc.DocumentNode.SelectSingleNode("//div[contains(@class, 'productInfo')]");
-			
-			var listingInfoNode = productInfoNode.SelectSingleNode("//div[contains(@class, 'listing')]");
-			var title = listingInfoNode.SelectSingleNode("//h1[@class='artist']").InnerHtml;
-			var songsRaw = listingInfoNode.SelectSingleNode("//h2[@class='tracks']").InnerHtml;
-			var songsList = songsRaw.Split("<br>").Select(s => s.Replace("\"", "")).ToList();
-
-			var playlistLabel = productInfoNode.SelectSingleNode("//div[@class='details']").SelectSingleNode("//p[contains(text(), 'Label')]/span/a").InnerHtml;
-
-			PlaylistName.Text = "Title : " + title;
-			PlaylistReleaseDate.Text = "Release Date : ";
-			PlaylistGenre.Text = "Genre : ";
-			PlaylistRecordLabel.Text = "Label : " + playlistLabel;
-
-			string artistAlbumSplitString = " - ";
-			var artistName = title[..title.IndexOf(artistAlbumSplitString, StringComparison.CurrentCultureIgnoreCase)];
-			var albumName = title.Remove(0, artistName.Length + artistAlbumSplitString.Length);
-
-			List<Song> trackList = songsList.Select(songTitle => new Song(songTitle, artistName, albumName)).ToList();
-
-			SongsList.Items = trackList.Select(t => new 
+			try
 			{
-				SongName = "Title : " + t.SongName,
-				ArtistName = "Artist : " + t.ArtistName,
-				AlbumName = "Album : " + t.AlbumName,
-				Duration = !t.Duration.IsNullOrWhiteSpace() ? "Duration : " + t.Duration : null,
-				Genre = !t.Genre.IsNullOrWhiteSpace() ? "Genre : " + t.Genre : null
-			});
+				var htmlDoc = new HtmlWeb().Load(PlaylistUrl.Text);
+				var playlistInfoNode = htmlDoc.DocumentNode.SelectSingleNode("//div[contains(@class, 'productInfo')]");
+				var trackListingInfoNode = GetPlayListInfo(playlistInfoNode, out string playlistTitle, out string playlistLabel, out Bitmap playlistImage);
 
-			//HtmlPageContent.Text = htmlDoc.ParsedText;
+				PlaylistImage.Source = playlistImage;
+				PlaylistName.Text = playlistTitle;
+				PlaylistReleaseDate.Text = null;
+				PlaylistGenre.Text = null;
+				PlaylistRecordLabel.Text = playlistLabel;
 
-			//var node = htmlDoc.DocumentNode.SelectSingleNode("//head/title");
-		}
-
-		private async Task<Bitmap> BitmapImageFromUrl(string url)
-		{
-			using (WebClient client = new WebClient())
-			{
-				byte[] imageBytes = await client.DownloadDataTaskAsync(url);
-
-				return new Bitmap(new MemoryStream(imageBytes));
+				TrackList.Items = GetTrackListFromNode(trackListingInfoNode, playlistTitle);
 			}
+			catch (Exception)
+			{
+				PlaylistUrl.BorderBrush = Brushes.Red;
+				PlaylistUrl.BorderThickness = new Thickness(3);
+				PlaylistUrl.Text = null;
+				ParseButton.IsEnabled = false;
+				isError = true; 
+				return;
+			}
+			finally
+			{
+				PlayListBioContainer.IsVisible = !isError;
+				this.Cursor = null;
+			}
+		}
+
+		private HtmlNode GetPlayListInfo(HtmlNode playlistInfoNode, out string playlistTitle, out string playlistLabel, out Bitmap playlistImage)
+		{
+			var listingInfoNode = playlistInfoNode.SelectSingleNode("//div[contains(@class, 'listing')]");
+
+			playlistImage =  BitmapImageFromUrl(playlistInfoNode.SelectSingleNode("//div[@class='h-100 image']/img").Attributes["src"].Value);
+			playlistTitle = listingInfoNode.SelectSingleNode("//h1[@class='artist']").InnerHtml;
+			playlistLabel = playlistInfoNode.SelectSingleNode("//div[@class='details']")
+				.SelectSingleNode("//p[contains(text(), 'Label')]/span/a").InnerHtml;
+			
+			return listingInfoNode;
+		}
+
+
+		private IEnumerable<Song> GetTrackListFromNode(HtmlNode listingNode, string playlistTitle)
+		{
+			var songsList = listingNode.SelectSingleNode("//h2[@class='tracks']").InnerHtml.Split("<br>")
+										.Select(s => s.Replace("\"", "")).ToList();
+
+			const string artistAlbumSplitString = " - ";
+			var artistName = playlistTitle[..playlistTitle.IndexOf(artistAlbumSplitString, StringComparison.CurrentCultureIgnoreCase)];
+			var albumName = playlistTitle.Remove(0, artistName.Length + artistAlbumSplitString.Length);
+
+
+			return songsList.Select(songTitle => new Song(songTitle, artistName, albumName)).ToList();
+		}
+
+		private Bitmap BitmapImageFromUrl(string url)
+		{
+			using WebClient client = new();
+			return new Bitmap(new MemoryStream(client.DownloadData(url)));
 		}
 
 		private void PlaylistUrl_OnGotFocus(object? sender, GotFocusEventArgs e)
@@ -85,9 +98,13 @@ namespace AvaloniaTest
 			{
 				while (true)
 				{
-					if (PlaylistUrl.Text is string playlistUri && !playlistUri.IsNullOrWhiteSpace())
+					if (!PlaylistUrl.Text.IsNullOrWhiteSpace())
 					{
-						Dispatcher.UIThread.InvokeAsync(() => { ParseButton.IsEnabled = true; });
+						Dispatcher.UIThread.InvokeAsync(() => { 
+							ParseButton.IsEnabled = true; 
+							PlaylistUrl.BorderBrush = Brushes.Black;
+							PlaylistUrl.BorderThickness = new Thickness(1);
+						});
 						break;
 					}
 					Thread.Sleep(100);
